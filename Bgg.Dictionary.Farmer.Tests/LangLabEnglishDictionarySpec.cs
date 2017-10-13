4f2a736050f4d37c1d719e4feca30d494c75b10b
Gomez Gimeno <VNP32I6@emea.vwg>
2017-10-13 15:33:13 +0200

    Added englishdictionary repository

---
+++ Bgg.Dictionary.Farmer.Tests/LangLabEnglishDictionarySpec.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Bgg.Dictionary.Farmer.Tests
+{
+    public class LangLabEnglishDictionarySpec
+    {
+        [Fact]
+        public async Task Can_insert_a_new_word()
+        {
+            await new LangLabEnglishDictionary().InsertWord(new Word("Hope"));
+
+        }
+    }
+}
---
+++ Bgg.Dictionary.Farmer/LangLabEnglishDictionary.cs
@@ -0,0 +1,30 @@
+﻿using Firebase.Database;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bgg.Dictionary.Farmer
+{
+    public class LangLabEnglishDictionary
+    {
+
+        public async Task InsertWord(Word word)
+        {
+            WebProxy proxy = new WebProxy("194.114.63.23:8080", true);
+            proxy.Credentials = new NetworkCredential("DQ18GR4", "67PaNn8$U9");
+            WebRequest.DefaultWebProxy = proxy;
+            //WebClient client = new WebClient();
+            //client.Proxy = proxy;
+            //return client.DownloadStringTaskAsync(new Uri(url));
+            FirebaseOptions opt = new FirebaseOptions();
+            var client = new FirebaseClient("https://langlab-1753c.firebaseio.com/");
+            var result = await client
+                .Child("englishDictionary")
+                .PostAsync(Newtonsoft.Json.JsonConvert.SerializeObject(word));
+            Console.WriteLine("stop here");
+
+        }
+    }
+}
